da023e0072dce92a1eac6759814e751ea876fdfd
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2024-09-23 12:35:00 +0700

    add bebel.config.js

---
+++ dist/babel.config.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: ["@babel/plugin-proposal-optional-chaining", "@babel/plugin-proposal-private-property-in-object"]
+};
+//# sourceMappingURL=babel.config.dev.js.map
